3407268053dcf466616e18d471813b244c8e0917
Fernando Crespo <fernando82@gmail.com>
2015-06-09 23:50:31 -0300

    Improved version

--- fuzzyclock.py
+++ fuzzyclock.py
@@ -5,62 +5,51 @@ import datetime
 import sys
 
 def parseHour(hour):
-    if hour in [1, 13]:
-        return "uma"
-    elif hour in [2, 14]:
-        return "duas"
-    elif hour in [3, 15]:
-        return "três"
-    elif hour in [4, 16]:
-        return "quatro"
-    elif hour in [5, 17]:
-        return "cinco"
-    elif hour in [6, 18]:
-        return "seis"
-    elif hour in [7, 19]:
-        return "sete"
-    elif hour in [8, 20]:
-        return "oito"
-    elif hour in [9, 21]:
-        return "nove"
-    elif hour in [10, 22]:
-        return "dez"
-    elif hour in [11, 23]:
-        return "onze"
-    elif hour in [0,24]:
-        return "meia noite"
-    elif hour in [12]:
-        return "meio dia"
+    hourList = ("meia noite", "uma", "duas", "tres", "quatro", "cinco", "seis",
+                "sete", "oito", "nove", "dez", "onze", "meio dia")
+    if hour > 23:
+        hour = 0
+    elif hour > 12:
+        hour = hour - 12
+    return hourList[hour]
+
+def junction(hour):
+    if hour <= 1 or hour == 13 or hour >= 23:
+        return "a"
+    elif hour == 12:
+        return "o"
+    else:
+        return "as"
 
 def parseMinute(hour, minute):
-    if minute >= 57 or minute < 3:
-        return parseHour(hour)
-    elif minute >= 3 and minute < 7:
+    if 3 <= minute < 8:
         return parseHour(hour) + " e cinco"
-    elif minute >= 7 and minute < 13:
+    elif 8 <= minute < 13:
         return parseHour(hour) + " e dez"
-    elif minute >= 13 and minute < 17:
+    elif 13 <= minute < 18:
         return parseHour(hour) + " e quinze"
-    elif minute >= 17 and minute < 23:
+    elif 18 <= minute < 23:
         return parseHour(hour) + " e vinte"
-    elif minute >= 23 and minute < 27:
+    elif 23 <= minute < 28:
         return parseHour(hour) + " e vinte e cinco"
-    elif minute >= 27 and minute < 33:
+    elif 28 <= minute < 33:
         return parseHour(hour) + " e meia"
-    elif minute >= 33 and minute < 37:
-        return "vinte e cinco para {0}{1}".format("as " if hour < 23 else "a ", parseHour(hour + 1))
-    elif minute >= 37 and minute < 43:
-        return "vinte para {0}{1}".format("as " if hour < 23 else "a ", parseHour(hour + 1))
-    elif minute >= 43 and minute < 47:
-        return "quinze para {0}{1}".format("as " if hour < 23 else "a ", parseHour(hour + 1))
-    elif minute >= 47 and minute < 53:
-        return "dez para {0}{1}".format("as " if hour < 23 else "a ", parseHour(hour + 1))
-    elif minute >= 53 and minute < 57:
-        return "cinco para {0}{1}".format("as " if hour < 23 else "a ", parseHour(hour + 1))
+    elif 33 <= minute < 38:
+        return "vinte e cinco para {0} {1}".format(junction(hour + 1), parseHour(hour + 1))
+    elif 38 <= minute < 43:
+        return "vinte para {0} {1}".format(junction(hour + 1), parseHour(hour + 1))
+    elif 43 <= minute < 48:
+        return "quinze para {0} {1}".format(junction(hour + 1), parseHour(hour + 1))
+    elif 48 <= minute < 53:
+        return "dez para {0} {1}".format(junction(hour + 1), parseHour(hour + 1))
+    elif 53 <= minute < 58:
+        return "cinco para {0} {1}".format(junction(hour + 1), parseHour(hour + 1))
+    else:
+        return parseHour(hour)
 
-def parseDate():
-    h=int(sys.argv[1]) if len(sys.argv) > 2 else datetime.datetime.now().time().hour
-    m=int(sys.argv[2]) if len(sys.argv) > 2 else datetime.datetime.now().time().minute
-    return parseMinute(h, m)
+def main():
+    h = int(sys.argv[1]) if len(sys.argv) > 2 else datetime.datetime.now().time().hour
+    m = int(sys.argv[2]) if len(sys.argv) > 2 else datetime.datetime.now().time().minute
+    print(parseMinute(h, m))
 
-print(parseDate())
+main()
